634a7fa9988f4757a4e66528608905e3571c8da7
Philipp <press.fm@mail.ru>
2022-12-08 16:56:17 +0300

    Выполнил упражнение_01

---
+++ Example_01_palindrom/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear ();
+
+Console.WriteLine("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 10000 || a > 99999)
+{
+    Console.Write("Попробуйте еще: ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+string s = a.ToString();
+
+char[] array = s.ToCharArray();
+
+for (int i = 0; i < 3;)
+{
+    if (array[i] == array [(array.Length - 1)])
+    {
+    if (array[i+1] == array [(array.Length - 2)]);
+    Console.Write ("Число является палиндромом");
+    break;
+    }
+    else
+    Console.Write ("Число не является палиндромом");
+    break;
+}
