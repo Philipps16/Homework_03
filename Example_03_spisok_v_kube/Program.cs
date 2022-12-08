2bf3e8f2faa1188fae64f9b776b9b7c26a1c8193
Philipp <press.fm@mail.ru>
2022-12-08 21:49:19 +0300

    Выполнил упражнение 03

---
+++ Example_03_spisok_v_kube/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число");
+
+int a = Convert.ToInt32 (Console.ReadLine ());
+if (a < 0)
+a = a * (-1);
+int b = 0;
+while (b < a)
+{
+    Console.Write ($"{Math.Pow (b, 3)}, ");
+    b++;
+}
